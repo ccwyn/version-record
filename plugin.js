8832f3c791078937f389ef3a948f97f289e21051
王亚男 <wangyanan01@renrenche.com>
2021-02-26 16:32:46 +0800

    feat: 版本结构修改

--- plugin.js
+++ plugin.js
@@ -1,13 +1,22 @@
 module.exports = function (babel, options) {
   const { types: t } = babel;
-  const { version } = options;
+  const { version, env } = options;
   return {
     name: "push elements in array",
     visitor: {
       VariableDeclarator(path) {
         const node = path.node;
-        if (node.id.name == "versions") {
-          node.init.elements = [ t.identifier(`"${version}"`),...node.init.elements];
+        if (node.id.name == "service_config") {
+          // 获取环境索引
+          let envIndex = node.init.properties.findIndex(item=>item.key.name===env)
+          // 获取版本号变量索引
+          let versionRecordIndex = node.init.properties[envIndex].value.properties.findIndex(item=>item.key.name==='versionRecord')
+          if(env==='development'){
+            node.init.properties[envIndex].value.properties[versionRecordIndex].value.elements=[t.identifier(`"${version}"`)];
+          }else{
+            node.init.properties[envIndex].value.properties[versionRecordIndex].value.elements.unshift(t.identifier(`"${version}"`));
+          }
+          // node.init.elements = [ t.identifier(`"${version}"`),...node.init.elements];
         }
       },
     },
